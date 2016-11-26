3bd079caa1690925d69a1a3d28b3348351a59536
18 Techno Chix <18technochix@gmail.com>
2016-11-26 12:22:59 -0500

    wip

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -0,0 +1,152 @@
+//teleop template
+
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by Techno Team_PC_III on 9/27/2015.
+ */
+
+/*
+fl = ly + rx + lx
+rl = ly + rx - lx
+fr = ly - rx - lx
+rr = ly - rx + lx
+ */
+
+
+// after class, replace "TeleOpTemplate" with your class/"program" name.
+@TeleOp(name="MecanumDrive", group="Linear Opmode")
+public class MecanumDrive extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+
+    DcMotor backLeft = null;
+    DcMotor backRight = null;
+    DcMotor frontLeft = null;
+    DcMotor frontRight = null;
+
+    /*
+
+        Create any sensors / motors here.
+
+        ex. TouchSensor frontTouch;
+
+        The first section is the type of object you are creating (ex. Motor, LightSensor, TouchSensor.
+        The second section is whatever you want to name that object.
+
+     */
+
+    public void runOpMode() throws InterruptedException{ //Initializes/runs the program.
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        backLeft = hardwareMap.dcMotor.get("bL");
+        backRight = hardwareMap.dcMotor.get("bR");
+        frontLeft = hardwareMap.dcMotor.get("fL");
+        frontRight = hardwareMap.dcMotor.get("fR");
+
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        /*
+
+            Initialize the sensors / motors here.
+
+            ex. frontTouch = hardwareMap.TouchSensor.get("name of sensor in the configuration");
+
+            the name of the sensor in the quotes will be whatever you call the sensor in
+            the configuration of the motors and sensors on the robot controller. it must be exactly
+            what you name it.
+
+         */
+
+        waitForStart(); //This waits for the driver station to say "start the program!".
+        runtime.reset();
+
+        while(opModeIsActive()) { //While the program is running, do this.
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+
+            double lx = gamepad1.left_stick_x;
+            double ly = gamepad1.left_stick_y;
+
+            double rx = gamepad1.right_stick_x;
+            double ry = gamepad1.right_stick_y;
+
+            /*
+            fl = ly + rx + lx
+            rl = ly + rx - lx
+            fr = ly - rx - lx
+            rr = ly - rx + lx
+            */
+
+
+
+
+            frontLeft.setPower(0.5*scale(ly, rx, lx));
+            backLeft.setPower(0.5*scale(ly, rx, -lx));
+            frontRight.setPower(0.5*scale(ly, -rx, -lx));
+            backRight.setPower(0.5*scale(ly, -rx, lx));
+
+            /*
+
+                Write any code here that allows the joysticks/buttons to control the motors and
+                sensors (if/while statments).
+
+                If you want to send information back to the driver station, you must submit it
+                through the telemetry.
+
+                ex. telemetry.addData("Front Touch Pressed:" , String.valueOf(frontTouch.isPressed()));
+
+                The value in the quotes is what the value will be named/displayed as on the phone.
+                After the comma is the number/value (which must be converted to a string) that
+                will change depending on the sensor.
+
+             */
+            waitForTick(40);
+            idle();
+        }
+
+    }
+
+    public double scale(double one, double two, double three){
+
+        if(one + two + three > 1){
+
+            return 1;
+
+        }else if(one + two + three < -1){
+
+            return -1;
+
+        }else{
+
+            return one + two + three;
+
+        }
+
+    }
+    public void waitForTick(long periodMs)  throws InterruptedException {
+
+        long  remaining = periodMs - (long)runtime.milliseconds();
+
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0)
+            Thread.sleep(remaining);
+
+        // Reset the cycle clock for the next pass.
+        runtime.reset();
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -0,0 +1,146 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Shooter", group="Linear Opmode")  // @Autonomous(...) is the other common choice
+public class Shooter extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+    DcMotor leftMotor = null;
+    DcMotor rightMotor = null;
+    double p=0.;
+    //...putting this up here so that I don't have to give it a value
+
+    //E848D2B812404A1465BC1A39910D6AF453AC938C3FF2B504486870E9FB0C0B51
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        /* eg: Initialize the hardware variables. Note that the strings used here as parameters
+         * to 'get' must correspond to the names assigned during the robot configuration
+         * step (using the FTC Robot Controller app on the phone).
+         */
+        leftMotor  = hardwareMap.dcMotor.get("left motor");
+        rightMotor = hardwareMap.dcMotor.get("right motor");
+
+        // eg: Set the drive motor directions:
+        // "Reverse" the motor that runs backwards when connected directly to the battery
+        leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        //i set the rightmotor to forwards from reverse bc of our tetrix/andymark config
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+
+
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+
+            /* figure out how to gradually increase power to desired power and then reverse once
+            button released, method loops, cycling every x ms */
+
+            double motorIncrement= .01;
+
+            if (gamepad1.y){
+                if (p < 1.) {
+                    p = p + motorIncrement;
+                }
+                else if (p >= 1.) {
+                    p = 1.;
+                    //this is here to stop the program from increasing power infinitely
+                    //is there a more efficient method?
+                }
+               /*theoretically: this code defines power variable p at the beginning of the program.
+               Then once it is initialized, it enters the if statement and since no buttons would
+               be pressed at initialization (hopefully? how to fix...) the statement would default to
+               the else and set p as 0. Once a button is pressed, the program would add
+                .01 (motorIncrement) every loop. Once p reaches a set value, the program keeps p
+                constant by setting it equal to max power desired.  When the button is released,
+                the program loops through the if statement and defaults to the else, gradually decreasing
+                p back to 0, then keeping it constant.
+                 */
+            }
+            else if (gamepad1.x){
+                //leaving this untouched so that if ^ does not work, there is less to do to fix it.
+                p=.75;
+            }
+            else if (gamepad1.a){
+                p=.5;
+            }
+            else if (gamepad1.b){
+                p=.25;
+            }
+            else{
+                  if (p > 0.){
+                      p = p - motorIncrement;
+                      if (p < 0.){
+                          p = 0.;
+                      }
+                  }
+                  else{ //p <= 0 ?
+                      p = 0;
+                  }
+            }
+
+            leftMotor.setPower(p);
+            rightMotor.setPower(p);
+
+            idle(); // Always call idle() at the bottom of your while(opModeIsActive()) loop
+        }
+    }
+}
