1073ba7fa32e0f81b7e38c17b3338a0f9cb58e2c
18 Techno Chix <18technochix@gmail.com>
2017-12-02 14:35:23 -0500

    created hardware class framework

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+//import statements
+import android.graphics.Color;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+
+/**
+ * Created by Techno Team_PC_III on 12/2/2017.
+ */
+
+@Disabled
+public class Hardware{
+    //public OpMode members
+
+    //constants
+
+    //local OpMode members
+    HardwareMap hwMap           =  null;
+    LinearOpMode opMode = null;
+    private Telemetry telemetry = null;
+
+    //constructor
+    public Hardware(LinearOpMode _opMode){
+        opMode = _opMode;
+        telemetry = _opMode.telemetry;
+    }
+
+
+    //init methods
+
+
+
+}
