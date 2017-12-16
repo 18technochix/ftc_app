072f3ec20ac0b799602d19b6a232ab2ab1a9ebea
18 Techno Chix <18technochix@gmail.com>
2017-12-16 17:06:49 -0500

    fixed moveThatRobot, simplified version as well

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -84,10 +84,17 @@ public class Hardware{
     int redPort = 1;
     static final int colorSampleMilliseconds = 48;
 
+    static final double     ENCODER_CPR             = 1120 ;
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;
+    static final double     COUNTS_PER_INCH         = (ENCODER_CPR * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+
     //local OpMode members
     HardwareMap hwMap           = null;
     LinearOpMode opMode         = null;
     private Telemetry telemetry = null;
+    public ElapsedTime runtime  = new ElapsedTime();
 
     //constructor
     public Hardware(LinearOpMode _opMode){
@@ -195,6 +202,140 @@ public void setRunMode(DcMotor.RunMode runMode){
     fr.setMode(runMode);
 }
 
+    public void moveThatRobot(double speed, double inches, double timeout){
+        moveThatRobot(speed, speed, speed, speed, inches, inches, inches, inches, timeout, "MOVE");
+    }
+
+    public void moveThatRobot(double frontRightSpeed, double backRightSpeed,
+                              double frontLeftSpeed, double backLeftSpeed,
+                              double frontRightInches, double backRightInches,
+                              double frontLeftInches,  double backLeftInches,
+                              double timeout, String tag){
+        int newFrontRightTarget;
+        int newBackRightTarget;
+        int newFrontLeftTarget;
+        int newBackLeftTarget;
+        double frCurrent;
+        double brCurrent;
+        double flCurrent;
+        double blCurrent;
+        double frComplete;
+        double brComplete;
+        double flComplete;
+        double blComplete;
+        double frSlowdown;
+        double brSlowdown;
+        double flSlowdown;
+        double blSlowdown;
+
+        //are we still running? good. if so:
+        if (opModeIsActive()) {
+            fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            //now, where do we go? let's set the target position.
+            newFrontRightTarget = fr.getCurrentPosition() + (int) (frontRightInches * Hardware.COUNTS_PER_INCH);
+            newBackRightTarget = br.getCurrentPosition() + (int) (backRightInches * Hardware.COUNTS_PER_INCH);
+            newFrontLeftTarget = fl.getCurrentPosition() + (int) (frontLeftInches * Hardware.COUNTS_PER_INCH);
+            newBackLeftTarget = bl.getCurrentPosition() + (int) (backLeftInches * Hardware.COUNTS_PER_INCH);
+            fr.setTargetPosition(newFrontRightTarget);
+            br.setTargetPosition(newBackRightTarget);
+            fl.setTargetPosition(newFrontLeftTarget);
+            bl.setTargetPosition(newBackLeftTarget);
+
+            //now you gotta make sure they know what to do with this info. give the motor a runmode.
+            fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            fr.setPower(Math.abs(frontRightSpeed));
+            br.setPower(Math.abs(backRightSpeed));
+            fl.setPower(Math.abs(frontLeftSpeed));
+            bl.setPower(Math.abs(backLeftSpeed));
+            double lastSpeedSetTime = runtime.seconds();
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeout) &&
+                    (fr.isBusy() || br.isBusy() || fl.isBusy() || bl.isBusy())) {
+
+                frCurrent = fr.getCurrentPosition();
+                brCurrent = br.getCurrentPosition();
+                flCurrent = fl.getCurrentPosition();
+                blCurrent = bl.getCurrentPosition();
+
+                // Display it for the driver.
+                //telemetry.addData(tag + ": Path1", "Running to %7d :%7d", newFrontRightTarget, newBackRightTarget, newFrontLeftTarget, newBackLeftTarget);
+                //telemetry.addData(tag + ": Path2", "Running at %7d :%7d", frCurrent, brCurrent, flCurrent, blCurrent);
+                //telemetry.update();
+
+                frComplete = (double) frCurrent / (double) newFrontRightTarget;
+                brComplete = (double) brCurrent / (double) newBackRightTarget;
+                flComplete = (double) flCurrent / (double) newFrontLeftTarget;
+                blComplete = (double) blCurrent / (double) newBackLeftTarget;
+
+                frSlowdown = 1.0;
+                if (frComplete > 0.92) {
+                    frSlowdown = 0.5;
+                } else if (frComplete > 0.85) {
+                    frSlowdown = 0.8;
+                }
+                brSlowdown = 1.0;
+                if (brComplete > 0.92) {
+                    brSlowdown = 0.5;
+                } else if (brComplete > 0.85) {
+                    brSlowdown = 0.8;
+                }
+                flSlowdown = 1.0;
+                if (flComplete > 0.92) {
+                    flSlowdown = 0.5;
+                } else if (flComplete > 0.85) {
+                    flSlowdown = 0.8;
+                }
+                blSlowdown = 1.0;
+                if (blComplete > 0.92) {
+                    blSlowdown = 0.5;
+                } else if (blComplete > 0.85) {
+                    blSlowdown = 0.8;
+                }
+
+                if ((runtime.seconds() - lastSpeedSetTime) > 0.04) {
+                    fr.setPower(frSlowdown * Math.abs(frontRightSpeed));
+                    br.setPower(brSlowdown * Math.abs(backRightSpeed));
+                    fl.setPower(flSlowdown * Math.abs(frontLeftSpeed));
+                    bl.setPower(blSlowdown * Math.abs(backLeftSpeed));
+                    lastSpeedSetTime = runtime.seconds();
+                }
+
+                opMode.idle();
+            }
+
+            // Stop all motion;
+            fr.setPower(0.);
+            br.setPower(0.);
+            fl.setPower(0.);
+            bl.setPower(0.);
+
+            // Turn off RUN_TO_POSITION
+            fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+
+        }
+    private boolean opModeIsActive(){return opMode.opModeIsActive();}
+
+    public double getHeading(){
+        Orientation angles = imu.getAngularOrientation().toAxesReference(AxesReference.INTRINSIC).toAxesOrder(AxesOrder.ZYX);
+        double angle = angles.firstAngle;
+        return AngleUnit.DEGREES.normalize(AngleUnit.DEGREES.fromUnit(angles.angleUnit, angle));
+    }
+    }
 
 
-}
