0d52c425e0e257137537d48dbd68a1ab34a08203
18 Techno Chix <18technochix@gmail.com>
2018-01-14 14:41:37 -0500

    found scale factor to allow robot to drive straight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFramework.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFramework.java
@@ -66,62 +66,69 @@ public class AutoFramework extends LinearOpMode {
         switch (autoType) {
             case AutoBlueAudience:
                 //knockDistance =
-                        jewelKnocker(robot.jewelServoBlue, 2, false);
+                //jewelKnocker(robot.jewelServoBlue, 2, false);
                 //park(35 + knockDistance, 0 , 0);
+                //centerOnTape(true);
+                testSpin();
                 break;
             case AutoBlueTimer:
                 //knockDistance =
-                        jewelKnocker(robot.jewelServoBlue, 2, false);
+                //jewelKnocker(robot.jewelServoBlue, 2, false);
                 // park(35 + knockDistance, 0 /*90*/ , 0);
+                centerOnTape(true);
                 break;
             case AutoRedAudience:
                 //knockDistance =
-                        jewelKnocker(robot.jewelServoRed, 1,true);
+                //jewelKnocker(robot.jewelServoRed, 1, true);
                 //  park(35 + knockDistance, 0 , 0);
+                centerOnTape(false);
                 break;
             case AutoRedTimer:
                 //knockDistance =
-                        jewelKnocker(robot.jewelServoRed, 1, true);
+                //jewelKnocker(robot.jewelServoRed, 1, true);
                 //  park(35 + knockDistance, 0 /*-90*/ , 0);
+                centerOnTape(false);
                 break;
         }
     }
 
 
-
-     public RelicRecoveryVuMark crytographReader() {
-     VuforiaTrackables relicTrackables = robot.picReader.loadTrackablesFromAsset("RelicVuMark");
-     VuforiaTrackable relicTemplate = relicTrackables.get(0);
-     relicTemplate.setName("relicVuMarkTemplate");
-     relicTrackables.activate();
-     RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
-     if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
-         OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) relicTemplate.getListener()).getPose();
-         if (pose != null) {
-             VectorF trans = pose.getTranslation();
-             Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
-             double tX = trans.get(0);
-             double tY = trans.get(1);
-             double tZ = trans.get(2);
-             double rX = rot.firstAngle;
-             double rY = rot.secondAngle;
-             double rZ = rot.thirdAngle;
-         }
-     }
-     RelicRecoveryVuMark side = vuMark;
-     telemetry.addData("VuMark", "%s visible", side);
-     telemetry.update();
-     return side;
-     }
+    public RelicRecoveryVuMark crytographReader() {
+        VuforiaTrackables relicTrackables = robot.picReader.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate");
+        relicTrackables.activate();
+        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
+            OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) relicTemplate.getListener()).getPose();
+            if (pose != null) {
+                VectorF trans = pose.getTranslation();
+                Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+                double tX = trans.get(0);
+                double tY = trans.get(1);
+                double tZ = trans.get(2);
+                double rX = rot.firstAngle;
+                double rY = rot.secondAngle;
+                double rZ = rot.thirdAngle;
+            }
+        }
+        RelicRecoveryVuMark side = vuMark;
+        telemetry.addData("VuMark", "%s visible", side);
+        telemetry.update();
+        return side;
+    }
 
 
     public double jewelKnocker(Servo allianceServo, int port, boolean alliance) {
         double hues[] = new double[6];
+
         double position = 0.25;
         for (int i = 0; i < 6; i++) {
             allianceServo.setPosition(position);
             sleep(50);
             double hue = robot.colorSensor.getHue(port);
+            telemetry.addData("Status", "Hue: " + hue);
+            telemetry.update();
             position -= 0.05;
             Array.set(hues, i, hue);
         }
@@ -129,62 +136,62 @@ public class AutoFramework extends LinearOpMode {
         for (int j = 0; j < 6; j++) {
             if (hues[j] > robot.redMin && hues[j] < robot.redMax) {
                 color = 1;
-            }
-            if (hues[j] > robot.blueMin && hues[j] < robot.blueMax) {
+            } else if (hues[j] > robot.blueMin && hues[j] < robot.blueMax) {
                 color = 2;
             }
         }
-            if (alliance && (color == 1)) {
-                robot.moveThatRobot(0.5, 2.0, 1.0);
-                retractBopper(allianceServo);
-                return -2.0;
-            }
-            if (alliance && (color == 2)) {
-                robot.moveThatRobot(0.5, -2.0, 1.0);
-                retractBopper(allianceServo);
-                return 2.0;
-            }
-            if (alliance && (color == 0)) {
-                retractBopper(allianceServo);
-                return 0.0;
-            }
-            if ((!alliance) && (color == 2)) {
-                robot.moveThatRobot(0.5, 2.0, 1.0);
-                retractBopper(allianceServo);
-                return -2.0;
-            }
-            if ((!alliance) && (color == 1)) {
-                robot.moveThatRobot(0.5, -2.0, 1.0);
-                retractBopper(allianceServo);
-                return 2.0;
-            }
-            if ((!alliance) && (color == 0)) {
-                retractBopper(allianceServo);
-                return 0.0;
-            }
+        if (alliance && (color == 1)) {
+            robot.moveThatRobot(0.5, 2.0, 1.0);
+            retractBopper(allianceServo);
+            return -2.0;
+        }
+        if (alliance && (color == 2)) {
+            robot.moveThatRobot(0.5, -2.0, 1.0);
+            retractBopper(allianceServo);
+            return 2.0;
+        }
+        if (alliance && (color == 0)) {
+            retractBopper(allianceServo);
             return 0.0;
+        }
+        if ((!alliance) && (color == 2)) {
+            robot.moveThatRobot(0.5, 2.0, 1.0);
+            retractBopper(allianceServo);
+            return -2.0;
+        }
+        if ((!alliance) && (color == 1)) {
+            robot.moveThatRobot(0.5, -2.0, 1.0);
+            retractBopper(allianceServo);
+            return 2.0;
+        }
+        if ((!alliance) && (color == 0)) {
+            retractBopper(allianceServo);
+            return 0.0;
+        }
+        return 0.0;
     }
 
 
-    public void retractBopper(Servo allianceServo){
+    public void retractBopper(Servo allianceServo) {
         allianceServo.setPosition(robot.JEWEL_UP);
         sleep(750);
     }
 
 
-        public void park(double distance){
-            robot.moveThatRobot(0.5, distance, 15.0);
-            sleep(50);
-            robot.fr.setPower(0.0);
-            robot.br.setPower(0.0);
-            robot.fl.setPower(0.0);
-            robot.bl.setPower(0.0);
+    public void park(double distance) {
+        robot.moveThatRobot(0.5, distance, 15.0);
+        sleep(50);
+        robot.fr.setPower(0.0);
+        robot.br.setPower(0.0);
+        robot.fl.setPower(0.0);
+        robot.bl.setPower(0.0);
 
-        }
-//two sides of the field: left = the pair of red and blue where the are across from eachother, and the pair where the are next to eachother
+    }
+
+    //two sides of the field: left = the pair of red and blue where the are across from eachother, and the pair where the are next to eachother
     //side by side: true
     //next to eachother: false
-        public void findTape(boolean side, boolean alliance, RelicRecoveryVuMark direction) {
+       /* public void findTape(boolean side, boolean alliance, RelicRecoveryVuMark direction) {
             double hue1 = robot.colorSensor.getHue(robot.tapeSensor1Port);
             double hue2 = robot.colorSensor.getHue(robot.tapeSensor2Port);
             boolean isColor = false;
@@ -226,15 +233,148 @@ public class AutoFramework extends LinearOpMode {
                 }
             }
         }
-
-   public void placeGlyph(){
+*/
+    public void placeGlyph() {
         //lower lift
         robot.glyphGrab.setPosition(robot.RELIC_GRAB_OPEN);
 
 
+    }
+
+    //switch()
+
+    // true side = side where the red and blue are next to eachother
+    //true alliance = blue
+    public void tapeFinder(boolean alliance, boolean side) {
+        //array of the colors of which the sensors pick up
+        double rightColor = robot.colorSensor.getHue(robot.tapeSensorRight);
+        double leftColor = robot.colorSensor.getHue(robot.tapeSensorLeft);
+        double tapeColors[] = {rightColor, leftColor};
+        //  boolean
+
+        if (alliance && side) {
+            //we are now facing the jewls, and for the sensors to be facing the cryptobox, the robot has to turn 180. But lets first roll of the balancing stone so its easier
+            robot.moveThatRobotSide(0.5, 0.5, 0.5, 0.5, 18, 2);
+            //now lets spin the robot to face it to the cryptobox
+            robot.spinRobot(180, 0.5);
+
+
+        }
+
+    }
+
+    public void centerOnTape(boolean alliance) {
+        boolean LeftreadingBlue = false;
+        boolean LeftreadingRed = false;
+        boolean RightreadingBlue = false;
+        boolean RightreadingRed = false;
+        double colorOfLeft = robot.colorSensor.getHue(robot.tapeSensorLeft);
+        double colorOfRight = robot.colorSensor.getHue(robot.tapeSensorRight);
+        if (colorOfLeft > robot.blueMin && colorOfLeft < robot.blueMax) {
+            LeftreadingBlue = true;
+        }
+        if (colorOfLeft > robot.redMin && colorOfLeft < robot.redMax) {
+            LeftreadingRed = true;
+        }
+        if (colorOfRight > robot.blueMin && colorOfRight < robot.blueMax) {
+            RightreadingBlue = true;
+        }
+        if (colorOfRight > robot.redMin && colorOfRight < robot.redMax) {
+            RightreadingRed = true;
+        }
+
+        if (alliance) {
+            if (LeftreadingBlue == true && RightreadingBlue == false) {
+                while (LeftreadingBlue == true && RightreadingBlue == false) {
+                    telemetry.addData("LeftColor", colorOfLeft);
+                    telemetry.addData("RightColor",colorOfRight);
+                    telemetry.update();
+                    moveLeft();
+                }
+            } else if (RightreadingBlue == true && LeftreadingBlue == true) {
+                while (RightreadingBlue == true && LeftreadingBlue == true) {
+                    telemetry.addData("LeftColor", colorOfLeft);
+                    telemetry.addData("RightColor",colorOfRight);
+                    telemetry.update();
+                    moveRight();
+
+                }
+            } else if (LeftreadingBlue == false && RightreadingBlue == false) ;
+            {
+                while (LeftreadingBlue == false && RightreadingBlue == false) {
+                    telemetry.addData("LeftColor", colorOfLeft);
+                    telemetry.addData("RightColor",colorOfRight);
+                    telemetry.update();
+                    robot.fr.setPower(0.5);
+                    robot.fl.setPower(0.5);
+                    robot.br.setPower(0.5);
+                    robot.bl.setPower(0.5);
+                }
+            }
+        } else if (!alliance) {
+            if (LeftreadingRed == true && RightreadingRed == false) {
+                while (LeftreadingRed == true && RightreadingRed == false) {
+                    telemetry.addData("LeftColor", colorOfLeft);
+                    telemetry.addData("RightColor",colorOfRight);
+                    telemetry.update();
+                    moveLeft();
+                }
+            } else if (RightreadingRed == true && LeftreadingRed == true) {
+                while (RightreadingRed == true && LeftreadingRed == true) {
+                    telemetry.addData("LeftColor", colorOfLeft);
+                    telemetry.addData("RightColor",colorOfRight);
+                    telemetry.update();
+                    moveRight();
+                }
+            } else if (LeftreadingRed == false && RightreadingRed == false) ;
+            {
+                while (LeftreadingRed == false && RightreadingRed == false) {
+                    telemetry.addData("LeftColor", colorOfLeft);
+                    telemetry.addData("RightColor",colorOfRight);
+                    telemetry.update();
+                    robot.fr.setPower(0.5);
+                    robot.fl.setPower(0.5);
+                    robot.br.setPower(0.5);
+                    robot.bl.setPower(0.5);
+                }
+
+            }
+
+
+        }
+    }
+
+    public void testSpin(){
+        //robot.setRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //robot.fr.setPower(0.5);
+        //robot.fl.setPower(-0.5);
+        //robot.br.setPower(0.5);
+        //robot.bl.setPower(-0.5);
+        //sleep(900);
+        //robot.fr.setPower(0.0);
+        //robot.fl.setPower(0.0);
+        //robot.br.setPower(0.0);
+        //robot.bl.setPower(0.0);
+        robot.moveThatRobot(0.6 * (1.0 + 12.0/70.0), 0.6, 120, 15);
+
+    }
+
+    public void moveRight() {
+        robot.fr.setPower(-0.5);
+        robot.br.setPower(0.5);
+        robot.fl.setPower(0.5);
+        robot.bl.setPower(-0.5);
+
+    }
+
+    public void moveLeft() {
+
+        robot.fl.setPower(-0.5);
+        robot.bl.setPower(0.5);
+        robot.fr.setPower(0.5);
+        robot.br.setPower(-0.5);
+}
 }
 
 
 
-
-}
